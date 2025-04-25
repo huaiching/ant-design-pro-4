b056df2957da6eb0ebf1d7c63f929996ee9cc288
huaiching <45527637+huaiching@users.noreply.github.com>
2025-04-25 14:10:34 +0800

    Update index.tsx

--- src/pages/AntdDemo/demo2_ProFormText/index.tsx
+++ src/pages/AntdDemo/demo2_ProFormText/index.tsx
@@ -1,111 +1,83 @@
-import { CaretDownFilled } from '@ant-design/icons';
+import { MliFormRow } from '@/common';
 import ProForm, { ProFormInstance, ProFormText } from '@ant-design/pro-form';
-import { FooterToolbar, PageContainer } from '@ant-design/pro-layout';
-import { Button, Dropdown, Menu, message } from 'antd';
+import { FooterToolbar } from '@ant-design/pro-layout';
+import { Button, message, Typography } from 'antd';
 import React, { useRef } from 'react';
 
 const MyForm: React.FC = () => {
-    const formRef = useRef<ProFormInstance>()
+  const formRef = useRef<ProFormInstance>()
 
-    const submitter = () => {
-        return {
-            render: () => (
-                <FooterToolbar>
-                    <Button
-                        type='primary'
-                        onClick={async () => {
-                            message.success("提交成功")
-                            fetch('http://localhost:8080/clnt/findAllById', {
-                                method: 'POST', // 發送 POST 請求
-                                headers: {      // 設置請求頭
-                                    'accept': '*/*',
-                                    'Content-Type': 'application/json'
-                                },
-                                body: JSON.stringify(
-                                    [
-                                        "A123456789"
-                                    ]
-                                )
-                            })
-                            .then(response => response.json())  // 解析 JSON 響應
-                            .then(data => {
-                                console.log('Success:', data);
-                                // 在這裡處理回傳的數據
-                            })
-                            .catch(error => {
-                                console.error('Error:', error);
-                                // 在這裡處理錯誤
-                            });
-                        }}
-                    >確認</Button>
-                    <Button
-                        onClick={async () => {
-                            message.warning("取消作業")
-                        }}
-                    >取消</Button>
-                </FooterToolbar>
-            )
-        }
-    }
-
-    return (
-        <>
-            {/* <h1>ProFormText</h1> */}
-            <PageContainer
-                header={{
-                    title: 'ProFormText',
-                    extra: [
-                        <Dropdown
-                            placement='bottom'
-                            trigger={['click']}
-                            overlay={(
-                                <Menu>
-                                    <Menu.Item
-                                        onClick={() => {
-                                            message.info('按了功能 1')
-                                        }}
-                                    >功能 1</Menu.Item>
-                                    <Menu.Item
-                                        onClick={() => {
-                                            message.info('按了功能 2')
-                                        }}
-                                    >功能 2</Menu.Item>
-                                </Menu>
-                            )}
-                        >
-                            <Button type='primary'>功能<CaretDownFilled /></Button>
-                        </Dropdown>
-                    ]
-                }}
+  // 控制送出後之動作
+  const submitterRender = () => {
+    return {
+      render: () => (
+        <FooterToolbar>
+            <Button
+              type="primary"
+              onClick={async () => {
+                formRef.current?.validateFields().then(values => {
+                  // 確認按鈕 點擊後 要進行的 API 操作
+                  message.success('表單提交成功！');
+                })
+              }}
+              key="save"
             >
-                <ProForm
-                    grid
-                    layout="vertical"
-                    formRef={formRef}
-                    submitter={submitter()}
-                >
-                    <ProFormText
-                        name="username"
-                        label="用戶名稱"
-                        tooltip="這是用戶名稱"
-                        placeholder="請輸入用戶名稱"
-                        colProps={{
-                            span: 6
-                        }}
-                        rules={[
-                            {
-                                required: true,
-                                message: '用戶名稱為必填項',
-                            },
-                        ]}
-                        fieldProps={{
-                            maxLength: 20
-                        }}
-                    />
-                </ProForm>
-            </PageContainer>
-        </>
-    )
+              確認
+            </Button>
+            <Button
+              onClick={async () => {
+                  // 取消按鈕 點擊後 要進行的 API 操作
+                  message.warning('取消作業');
+              }}
+            >
+              取消
+            </Button>
+        </FooterToolbar>
+      )
+    }
+  }
+
+  const vaildatorEmail = (rule: any, value: any) => {
+    const re = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/
+    if (!re.test(value)) {
+      return Promise.reject("不符合規範!!")
+    }
+    return Promise.resolve()
+  }
+
+  return (
+    <>
+      <Typography.Title level={3}>ProFormText</Typography.Title>
+      <ProForm
+        grid
+        layout="vertical"
+        formRef={formRef}
+        submitter={submitterRender()}
+      >
+        <MliFormRow>
+          <ProFormText
+            name="email"
+            label="電子郵件"
+            tooltip="這是用戶電子郵件"
+            placeholder="請輸入電子郵件"
+            colSize={1}
+            rules={[
+              {
+                  required: true,
+                  message: '必填',
+              },
+              {
+                validator: vaildatorEmail
+              },
+            ]}
+            fieldProps={{
+                maxLength: 72,
+            }}
+          />
+        </MliFormRow>
+      </ProForm>
+    </>
+  )
 };
 
 export default MyForm;
