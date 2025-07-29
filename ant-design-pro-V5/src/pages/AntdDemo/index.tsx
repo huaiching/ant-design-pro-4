0e97d07c19279b472ed50dd5b434f438d3ecec0c
ray0305 <ray0305@gmail.com>
2025-07-30 06:12:33 +0800

    no message

--- ant-design-pro-V5/src/pages/AntdDemo/index.tsx
+++ ant-design-pro-V5/src/pages/AntdDemo/index.tsx
@@ -10,57 +10,79 @@ import Demo07 from './Components/PageTemplates'
 import { PageContainer } from '@ant-design/pro-components'
 import { Tabs } from 'antd'
 import TabPane from 'antd/es/tabs/TabPane'
+import { useNavigate, useSearchParams } from 'umi';
 
 //asstManagement 主功能名稱
 const AsstManagement: React.FC = () => {
   //主功能9000代碼
   const pageTitle = '組件測試頁面'
+  const navigate = useNavigate();
+  const [searchParams] = useSearchParams();
+  
 
   //設定tabs 頁面元件
   //主頁主要設定處
   const tabs = [
     {
       authCode: '1',
+      key: 'Form',
       title: '表單輸入元件(Form)',
       component: <Demo01/>
     },
     {
       authCode: '2',
+      key: 'Container',
       title: '表單容器(Container)',
       component: <Demo02/>
     },
     {
       authCode: '3',
+      key: 'Table',
       title: '表格(Table)',
       component: <Demo03/>
     },
     {
       authCode: '4',
+      key: 'LayoutUi',
       title: '佈局與視覺元件(LayoutUi)',
       component: <Demo04/>
     },
     {
       authCode: '5',
+      key: 'DataDisplay',
       title: '資料顯示元件(DataDisplay)',
       component: <Demo05/>
     },
     {
       authCode: '6',
+      key: 'Utility',
       title: '工具類與整合範例(Utility)',
       component: <Demo06/>
     },
     {
       authCode: '7',
+      key: 'PageTemplates',
       title: '頁面樣板(PageTemplates)',
       component: <Demo07/>
     },
   ]
   
+  // 取得當前 activeKey，若沒有就預設為第一個 tab 的 key
+  const currentActiveKey = searchParams.get('activeKey') || tabs[0].key;
+  
   return (
     <PageContainer title={pageTitle}>
-      <Tabs type='card' size='large'>
+      <Tabs 
+        type='card' size='large' 
+        activeKey={currentActiveKey}
+        onChange={(key: string) => {
+          navigate({
+            search: `?activeKey=${key}`,
+          });
+        }}
+      >
         {tabs.map((item) => (
-          <TabPane tab={item.title} key={item.authCode}>
+          <TabPane tab={item.title} key={item.key}>
             {item.component}
           </TabPane>
         ))}
