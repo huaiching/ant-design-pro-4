2c39f7d20c9a86a81fc2b88cc82666ed544a8ad4
ray0305 <ray0305@gmail.com>
2025-09-04 19:58:25 +0800

    Reapply "Update index.tsx"
    This reverts commit 83a11a26cb47071b9a7ece03496fd3e23e8aadbc.

--- ant-design-pro-V5/src/pages/AntdDemo/components/Table/Components/demo02_SearchProTable/index.tsx
+++ ant-design-pro-V5/src/pages/AntdDemo/components/Table/Components/demo02_SearchProTable/index.tsx
@@ -8,12 +8,13 @@
  *    3. total  : 數據筆數
  */
 
+import { ProFormInstance } from '@ant-design/pro-form'
+import ProTable, { ActionType, ProColumns } from '@ant-design/pro-table'
+import { List, Space } from 'antd'
+import dayjs from 'dayjs'
 import React, { useRef } from 'react'
 import * as userApi from './store/userApi'
-import ProTable, { ActionType, ProColumns } from '@ant-design/pro-table'
-import { ProFormInstance } from '@ant-design/pro-form'
-import { List, Space, Typography } from 'antd'
-import dayjs from 'dayjs'
+import { parseRocDate } from '@/utils/rocDateUtils'
 
 const ProTableDemo: React.FC = () => {
   const formRef = useRef<ProFormInstance>()
@@ -37,10 +38,15 @@ const ProTableDemo: React.FC = () => {
       dataIndex: 'option',
       valueType: 'option',
       render: (dom, entity) => [
-        <a key='a' onClick={() => {
-          console.info('dom', dom)
-          console.info('entity', entity)
-        }}>明細</a>
+        <a
+          key="a"
+          onClick={() => {
+            console.info('dom', dom)
+            console.info('entity', entity)
+          }}
+        >
+          明細
+        </a>
       ]
     },
     {
@@ -81,7 +87,13 @@ const ProTableDemo: React.FC = () => {
       dataIndex: 'birthDate',
       valueType: 'date',
       fieldProps: {
-        format: 'TTT/MM/DD'
+        format: 'TTT/MM/DD',
+        onBlur: (e: any) => {
+          // 日期格式化
+          const date = parseRocDate(e.target?.value)
+          // 更新資料
+          formRef.current?.setFieldValue('birthDate', date)
+        }
       }
     }
   ]
@@ -89,14 +101,14 @@ const ProTableDemo: React.FC = () => {
   return (
     <>
       <ProTable
-        name='testTable'
+        name="testTable"
         columns={columns}
-        formRef={formRef}       // 查詢框 的 欄位變數
-        actionRef={actionRef}   // 表格控制的變數實體
+        formRef={formRef} // 查詢框 的 欄位變數
+        actionRef={actionRef} // 表格控制的變數實體
         request={async (params) => {
           const res = await userApi.fetchAllData(params)
           // 日期格式轉換
-          const chgData = res.data.map(e => ({
+          const chgData = res.data.map((e) => ({
             ...e,
             birthDate: dayjs(e.birthDate, 'TTT/MM/DD')
           }))
@@ -105,25 +117,23 @@ const ProTableDemo: React.FC = () => {
             success: true,
             total: chgData.length
           }
-        }}   // 數據請求函式
-        manualRequest={true}    // 手動請求數據
-        rowKey='id'             // 設定 資料唯一值 欄位
+        }} // 數據請求函式
+        manualRequest={true} // 手動請求數據
+        rowKey="id" // 設定 資料唯一值 欄位
         search={{
           labelWidth: 'auto'
         }}
         form={{
           ignoreRules: false
         }}
-        headerTitle='模擬 API 表格'
-        toolBarRender={() => [
-          <div key='d'>toolBarRender</div>
-        ]}
+        headerTitle="模擬 API 表格"
+        toolBarRender={() => [<div key="d">toolBarRender</div>]}
         options={{
-          density: true,    // 密度
+          density: true, // 密度
           fullScreen: true, // 全螢幕
-          reload: true,     // 刷新
-          setting: true,    // 列設置
-          search: true      // 搜尋欄
+          reload: true, // 刷新
+          setting: true, // 列設置
+          search: true // 搜尋欄
         }}
         pagination={{
           showQuickJumper: true
@@ -133,32 +143,37 @@ const ProTableDemo: React.FC = () => {
           type: 'checkbox'
         }}
         tableAlertRender={({
+          // eslint-disable-next-line @typescript-eslint/no-unused-vars
           selectedRowKeys, // 選取行的key
-          selectedRows     // 選取行的資料
+          selectedRows // 選取行的資料
         }) => {
           // 生日 轉換為 string
-          const exportData = selectedRows.map(row => ({
+          const exportData = selectedRows.map((row) => ({
             ...row,
             birthDate: dayjs(row.birthDate).format('TTT/MM/DD') // 將日期轉為 string
-          }));
+          }))
 
           return (
             <Space size={16}>
-              <a onClick={() => {
-                console.info(exportData); // 這裡的 birthDate 已是字串
-              }}>導出數據</a>
+              <a
+                onClick={() => {
+                  console.info(exportData) // 這裡的 birthDate 已是字串
+                }}
+              >
+                導出數據
+              </a>
             </Space>
-          );
+          )
         }}
       />
       <List
-        size='small'
+        size="small"
         dataSource={[
-          '1. Date: 日期格式 fieldProps.format 設定為 \'TTT/MM/DD\' (民國年)。',
-          '2. 前端日期資料 (string) 要轉換為 dayjs 物件時，請使用 dayjs(XXX, \'TTT/MM/DD\') 進行格式轉換。',
-          '3. 導出數據時，要使用 dayjs(XXX).format(\'TTT/MM/DD\') 來將 日期 轉換為 string',
+          "1. Date: 日期格式 fieldProps.format 設定為 'TTT/MM/DD' (民國年)。",
+          "2. 前端日期資料 (string) 要轉換為 dayjs 物件時，請使用 dayjs(XXX, 'TTT/MM/DD') 進行格式轉換。",
+          "3. 導出數據時，要使用 dayjs(XXX).format('TTT/MM/DD') 來將 日期 轉換為 string"
         ]}
-        renderItem={item => <List.Item>{item}</List.Item>}
+        renderItem={(item) => <List.Item>{item}</List.Item>}
       />
     </>
   )
