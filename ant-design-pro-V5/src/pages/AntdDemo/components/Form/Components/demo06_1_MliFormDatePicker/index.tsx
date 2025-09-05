ef4ce4668b545758e9a2c49200354da179db9a29
huaiching <45527637+huaiching@users.noreply.github.com>
2025-09-05 14:23:41 +0800

    Update index.tsx

--- ant-design-pro-V5/src/pages/AntdDemo/components/Form/Components/demo06_1_MliFormDatePicker/index.tsx
+++ ant-design-pro-V5/src/pages/AntdDemo/components/Form/Components/demo06_1_MliFormDatePicker/index.tsx
@@ -5,12 +5,12 @@
  *    moduleName.columns.columnName: 'label顯示的名稱'
  */
 
-import React, { useRef } from 'react'
+import { parseRocDate, parseRocDateMonth } from '@/utils/rocDateUtils'
 import ProForm, { ProFormInstance } from '@ant-design/pro-form'
 import { FooterToolbar } from '@ant-design/pro-layout'
-import { Button, List, message, Typography } from 'antd'
 import { MliFormDatePicker, MliFormDateRangePicker, MliFormRow } from '@mli-csmo/base'
-import { parseRocDate, parseRocDateMonth } from '@/utils/rocDateUtils'
+import { Button, List, message } from 'antd'
+import React, { useRef } from 'react'
 
 const MyForm: React.FC = () => {
   const formRef = useRef<ProFormInstance>()
@@ -21,7 +21,7 @@ const MyForm: React.FC = () => {
       render: () => (
         <FooterToolbar>
           <Button
-            type='primary'
+            type="primary"
             onClick={async () => {
               formRef.current?.validateFields().then(() => {
                 // 確認按鈕 點擊後 要進行的 API 操作
@@ -29,7 +29,7 @@ const MyForm: React.FC = () => {
                 message.success('表單提交成功！')
               })
             }}
-            key='save'
+            key="save"
           >
             確認
           </Button>
@@ -46,100 +46,82 @@ const MyForm: React.FC = () => {
     }
   }
 
-
   return (
     <>
       <h1>MliFormDatePicker</h1>
-      <ProForm
-        grid
-        layout='vertical'
-        formRef={formRef}
-        submitter={submitterRender()}
-      >
+      <ProForm grid layout="vertical" formRef={formRef} submitter={submitterRender()}>
         <MliFormRow>
           <MliFormDatePicker
-            name='chkDate'
-            moduleName='mliFormDate'
-            columnName='chkDate'
+            name="chkDate"
+            moduleName="mliFormDate"
+            columnName="chkDate"
             // label='選擇日期'
-            placeholder='請選擇日期'
+            placeholder="請選擇日期"
             fieldProps={{
               onBlur: (e: any) => {
-                // 日期格式化
-                const date = parseRocDate(e.target?.value)
-                // 更新資料
-                formRef.current?.setFieldsValue({
-                  chkDate: date
-                })
+                if (e.target?.value) {
+                  formRef.current?.setFieldValue('chkDate', parseRocDate(e.target?.value))
+                }
               }
             }}
-            rules={[
-              { required: true, message: '日期為必填項' }
-            ]}
+            rules={[{ required: true, message: '日期為必填項' }]}
           />
           <MliFormDatePicker
-            name='chkDateYYMM'
-            moduleName='mliFormDate'
-            columnName='chkDateYYMM'
+            name="chkDateYYMM"
+            moduleName="mliFormDate"
+            columnName="chkDateYYMM"
             // label='選擇月份'
-            placeholder='請選擇月份'
-            rules={[
-              { required: true, message: '日期為必填項' }
-            ]}
+            placeholder="請選擇月份"
+            rules={[{ required: true, message: '日期為必填項' }]}
             fieldProps={{
               picker: 'month',
               format: 'TTT/MM',
               onBlur: (e: any) => {
-                // 日期格式化
-                const date = parseRocDateMonth(e.target?.value)
-                // 更新資料
-                formRef.current?.setFieldsValue({
-                  chkDateYYMM: date
-                })
+                if (e.target?.value) {
+                  formRef.current?.setFieldValue('chkDateYYMM', parseRocDateMonth(e.target?.value))
+                }
               }
             }}
           />
           <MliFormDateRangePicker
-            name='chkDateRange'
-            moduleName='mliFormDate'
-            columnName='chkDateRange'
+            name="chkDateRange"
+            moduleName="mliFormDate"
+            columnName="chkDateRange"
             // label='選擇日期區間'
-            rules={[
-              { required: true, message: '日期為必填項' }
-            ]}
+            rules={[{ required: true, message: '日期為必填項' }]}
             fieldProps={{
               onBlur: (e: any) => {
-                const root = e.target?.closest('.ant-picker-range')
-                if (!root) return
+                if (e.target?.value) {
+                  const root = e.target?.closest('.ant-picker-range')
+                  if (!root) return
 
-                // 抓兩個 input 的原始字串
-                const inputs = root.querySelectorAll('input')
-                const startRaw = inputs?.[0]?.value ?? ''
-                const endRaw = inputs?.[1]?.value ?? ''
+                  // 抓兩個 input 的原始字串
+                  const inputs = root.querySelectorAll('input')
+                  const startRaw = inputs?.[0]?.value ?? ''
+                  const endRaw = inputs?.[1]?.value ?? ''
 
-                // 日期格式化
-                const start = parseRocDate(startRaw)
-                const end = parseRocDate(endRaw)
+                  // 日期格式化
+                  const start = parseRocDate(startRaw)
+                  const end = parseRocDate(endRaw)
 
-                // 回寫到表單
-                formRef.current?.setFieldsValue({
-                  chkDateRange: [start, end]
-                })
+                  // 回寫到表單
+                  formRef.current?.setFieldValue('chkDateRange', [start, end])
+                }
               }
             }}
           />
         </MliFormRow>
         <List
-          size='small'
+          size="small"
           dataSource={[
-            '1. Date: 日期格式 fieldProps.format 設定為 \'TTT/MM/DD\' (民國年)。',
-            '2. 前端日期資料 (string) 要轉換為 dayjs 物件時，請使用 dayjs(XXX, \'TTT/MM/DD\') 進行格式轉換。',
-            '3. 導出數據時，要使用 dayjs(XXX).format(\'TTT/MM/DD\') 來將 日期 轉換為 string',
+            "1. Date: 日期格式 fieldProps.format 設定為 'TTT/MM/DD' (民國年)。",
+            "2. 前端日期資料 (string) 要轉換為 dayjs 物件時，請使用 dayjs(XXX, 'TTT/MM/DD') 進行格式轉換。",
+            "3. 導出數據時，要使用 dayjs(XXX).format('TTT/MM/DD') 來將 日期 轉換為 string",
             '4. 使用 MliFormDatePicker 會直接使用 民國年 (TTT/MM/DD)。',
             '5. 需要設定 moduleName 和 columnName，並且 欄位中文 要透過 國際化文件 取得 (moduleName.columns.columnName)。',
-            '6. 月份 跟 年份 元件 透過 fieldProps.picker 設定，如：月份 為 fieldProps.picker: \'month\'。',
+            "6. 月份 跟 年份 元件 透過 fieldProps.picker 設定，如：月份 為 fieldProps.picker: 'month'。"
           ]}
-          renderItem={item => <List.Item>{item}</List.Item>}
+          renderItem={(item) => <List.Item>{item}</List.Item>}
         />
       </ProForm>
     </>
