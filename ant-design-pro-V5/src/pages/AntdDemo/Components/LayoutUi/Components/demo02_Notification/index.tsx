2af0c8141265e67a5ec215c4c92fe4f829beea9a
ray0305 <ray0305@gmail.com>
2025-09-07 18:40:26 +0800

    feat: 訊息工具化

--- ant-design-pro-V5/src/pages/AntdDemo/Components/LayoutUi/Components/demo02_Notification/index.tsx
+++ ant-design-pro-V5/src/pages/AntdDemo/Components/LayoutUi/Components/demo02_Notification/index.tsx
@@ -1,35 +1,30 @@
 import React from 'react'
-import { Button, notification } from 'antd'
+import { Button, notification, Space } from 'antd'
+import { notificationSuccess, notificationInfo, notificationWarning, notificationError } from './Store/NotificaionStore'
 
 const App: React.FC = () => {
   const data = [
-    '錯誤內容',
+    '第一行訊息',
     '第二行訊息',
     '第三行訊息',
     '第四行訊息'
   ]
 
-  const openNotificationWithMultipleLines = (msgList: string[]) => {
-    // 將 訊息集合 轉換為 html標籤
-    const description = msgList.map((msg) => (
-      <p key='msg'>{msg}</p>
-    ))
-    // 顯示 通知提醒框
-    notification['error']({         // success / info / warning / error
-      message: '我是標題',           // 標題
-      // description: '錯誤內容',    // 內文 單行直接給文字，多行可以用 html標籤
-      description: description,
-      duration: 0,                  // 幾秒後自動關閉，0=不自動關閉
-      placement: 'top'              // 顯示位置 'top' | 'topLeft' | 'topRight'(預設) | 'bottom' | 'bottomLeft' | 'bottomRight'
-    })
-  }
-
   return (
-    <div>
-      <Button type='primary' onClick={() => openNotificationWithMultipleLines(data)}>
-        顯示通知訊息
+    <Space>
+      <Button type='primary' onClick={() => notificationSuccess(data)}>
+        success
       </Button>
-    </div>
+      <Button type='primary' onClick={() => notificationInfo(data)}>
+        info
+      </Button>
+      <Button type='primary' onClick={() => notificationWarning(data)}>
+        warning
+      </Button>
+      <Button type='primary' onClick={() => notificationError(data)}>
+        error
+      </Button>
+    </Space>
   )
 }
 
--- ant-design-pro-V5/src/pages/AntdDemo/Components/LayoutUi/Components/demo03_Modal/index.tsx
+++ ant-design-pro-V5/src/pages/AntdDemo/Components/LayoutUi/Components/demo03_Modal/index.tsx
@@ -1,43 +1,34 @@
 import React from 'react'
-import { Button, message, Modal } from 'antd'
+import { Button, message, Modal, Space } from 'antd'
+import { modalWarning, modalInfo, modalError, modalSuccess, modalConfirm } from './Store/ModalStore'
 
 const App: React.FC = () => {
   const data = [
-    '請問是否要繼續？',
+    '第一行訊息',
     '第二行訊息',
     '第三行訊息',
     '第四行訊息'
   ]
 
-  // 對話框，可用於 顯示 確認訊息
-  const openModal = (msgList: string[]) => {
-    // 將 訊息集合 轉換為 html標籤
-    const description = msgList.map((msg) => (
-      <p key='msg'>{msg}</p>
-    ))
-    // 顯示 對話框
-    Modal.confirm({                    // confirm | warning | info | error
-      title: '標題文字',                // 標題文字
-      // content: '請問是否要繼續？',    // 內文 單行直接給文字，多行可以用 html標籤
-      content: description,
-      okText: '繼續',                   // 複寫 確定 的文字 (預設 確定)
-      cancelText: '放棄',               // 複寫 取消 的文字 (預設 取消)
-      onOk() {                          // 確定 要做的事情
-        message.success('繼續作業')
-      },
-      onCancel() {                      // 取消 要做的事情
-        message.error('放棄作業')
-      }
-    })
-  }
-
 
   return (
-    <>
-      <Button type='primary' onClick={()=>openModal(data)}>
-        顯示確認訊息
+    <Space>
+      <Button type='primary' onClick={()=>modalConfirm(data)}>
+        confirm
       </Button>
-    </>
+      <Button type='primary' onClick={()=>modalSuccess(data)}>
+        success
+      </Button>
+      <Button type='primary' onClick={()=>modalWarning(data)}>
+        warning
+      </Button>
+      <Button type='primary' onClick={()=>modalInfo(data)}>
+        info
+      </Button>
+      <Button type='primary' onClick={()=>modalError(data)}>
+        error
+      </Button>
+    </Space>
   )
 }
 
