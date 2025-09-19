9b614ea8a7970f41f5725b6abdc44e0ac7f053c8
huaiching <45527637+huaiching@users.noreply.github.com>
2025-09-19 16:59:12 +0800

    Update poApi.ts

--- ant-design-pro-V5/src/pages/AntdDemo/Components/Table/Components/demo13_CheckboxNestedEditableProTable/store/poApi.ts
+++ ant-design-pro-V5/src/pages/AntdDemo/Components/Table/Components/demo13_CheckboxNestedEditableProTable/store/poApi.ts
@@ -4,10 +4,11 @@ export const fetchAllData = async (): Promise<any[]> => {
   const data = Array.from({ length: 50 }, (_, i) => {
     const id = i + 1
     return {
-      key: String(id),
-      policyNo: `P20250716${id.toString().padStart(3, '0')}`,
-      poStsCode: id % 2 === 0 ? '有效' : '失效',
-      poIssueDate: `114/01/${(id % 30 + 1).toString().padStart(2, '0')}`,
+      policyNo: `173200785${id.toString().padStart(3, '0')}`,
+      coInfoList: [
+        {planCodeDesc: `A${id.toString().padStart(3, '0')}-1`},
+        {planCodeDesc: `B${id.toString().padStart(3, '0')}-2`}
+      ]
     }
   })
 
