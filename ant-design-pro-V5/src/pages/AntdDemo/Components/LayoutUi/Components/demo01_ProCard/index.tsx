78edb4b58859401e96ec02a4229496d43f3e9503
ray0305 <ray0305@gmail.com>
2025-09-20 09:12:53 +0800

    feat: 修改 ProCard 範例

--- ant-design-pro-V5/src/pages/AntdDemo/Components/LayoutUi/Components/demo01_ProCard/index.tsx
+++ ant-design-pro-V5/src/pages/AntdDemo/Components/LayoutUi/Components/demo01_ProCard/index.tsx
@@ -1,48 +1,53 @@
 import ProCard from '@ant-design/pro-card'
 import ProForm, { ProFormInstance, ProFormText } from '@ant-design/pro-form'
-import { Button, message, Segmented } from 'antd'
+import { Button, ConfigProvider, message, Segmented, Space, Typography } from 'antd'
 import React, { useEffect, useRef, useState } from 'react'
-import { editOption } from './store/editOption'
 import { FooterToolbar } from '@ant-design/pro-layout'
 import { MliFormRow } from '@mli-csmo/base'
+import { CloseOutlined, EditOutlined } from '@ant-design/icons'
+
+const editOption = [
+  { value: 'edit', icon: <EditOutlined /> },
+  { value: 'disabled', icon: <CloseOutlined /> }
+]
 
 const InsurancePolicyCard: React.FC = () => {
   const formRef = useRef<ProFormInstance>()
-  const [poEdit, setPoEdit] = useState<boolean>(false)
+  const [poEdit, setPoEdit] = useState<string>('disabled')
 
-  useEffect(()=>{
-      formRef.current?.setFieldsValue({
-          policyNo: '1234567890',
-          poStsCode: '42'
-      })
-  },[])
+  useEffect(() => {
+    formRef.current?.setFieldsValue({
+      policyNo: '1234567890',
+      poStsCode: '42'
+    })
+  }, [])
 
   // 控制送出後之動作
   const submitterRender = () => {
     return {
       render: () => (
         <FooterToolbar>
-            <Button
-              type='primary'
-              onClick={async () => {
-                formRef.current?.validateFields().then(() => {
-                  // 確認按鈕 點擊後 要進行的 API 操作
-                  message.success('表單提交成功！')
-                  console.info('formRef', formRef.current?.getFieldsValue())
-                })
-              }}
-              key='save'
-            >
-              確認
-            </Button>
-            <Button
-              onClick={async () => {
-                  // 取消按鈕 點擊後 要進行的 API 操作
-                  message.warning('取消作業')
-              }}
-            >
-              取消
-            </Button>
+          <Button
+            type='primary'
+            onClick={async () => {
+              formRef.current?.validateFields().then(() => {
+                // 確認按鈕 點擊後 要進行的 API 操作
+                message.success('表單提交成功！')
+                console.info('formRef', formRef.current?.getFieldsValue())
+              })
+            }}
+            key='save'
+          >
+            確認
+          </Button>
+          <Button
+            onClick={async () => {
+              // 取消按鈕 點擊後 要進行的 API 操作
+              message.warning('取消作業')
+            }}
+          >
+            取消
+          </Button>
         </FooterToolbar>
       )
     }
@@ -56,34 +61,42 @@ const InsurancePolicyCard: React.FC = () => {
       submitter={submitterRender()}
     >
       <ProCard
-        title='保單資訊'
-        type='default'
-        size='default'
+        // title='保單資訊'
+        title={
+          <Space wrap align="center">
+            <Segmented
+              value={poEdit}
+              options={editOption}
+              onChange={(value)=>{
+                setPoEdit(value)
+                message.info('按了'+value)
+              }}
+              size='large'
+            />
+            <Typography.Title level={4}>保單資訊</Typography.Title>
+          </Space>
+        }
+        type='inner'
+        size='small'
         headerBordered      // 有 分隔線
         collapsible         // 有 摺疊
         defaultCollapsed    // 預設 折疊
         extra={
-          // 分段控制器: 這裡用來控制 編輯 的切換
-          <Segmented
-            options={editOption}        // 分頁控制器的內容設定 (預設選擇第一個)
-            onChange={(value) => {      // 切換事件：value=切換後的數值
-              setPoEdit(value === 'edit')
-            }}
-          />
+          <Typography.Text>extra</Typography.Text>
         }
       >
-        <MliFormRow>
-          <ProFormText
-            name='policyNo'
-            label='保單號碼'
-            readonly={!poEdit}
-          />
-          <ProFormText
-            name='poStsCode'
-            label='保單狀態'
-            readonly={!poEdit}
-          />
-        </MliFormRow>
+        <ConfigProvider componentDisabled={poEdit !== 'edit'}>
+          <MliFormRow>
+            <ProFormText
+              name='policyNo'
+              label='保單號碼'
+            />
+            <ProFormText
+              name='poStsCode'
+              label='保單狀態'
+            />
+          </MliFormRow>
+        </ConfigProvider>
       </ProCard>
     </ProForm>
   )
