3029ccc31d119ba923740a0121a4d859a520d1b7
huaiching <45527637+huaiching@users.noreply.github.com>
2026-01-30 09:48:38 +0800

    Update index.tsx

--- csmo-web-demo/packages/app-demo/src/pages/AntdDemo/Demo/Form/Components/demo02_ProFormText/index.tsx
+++ csmo-web-demo/packages/app-demo/src/pages/AntdDemo/Demo/Form/Components/demo02_ProFormText/index.tsx
@@ -1,26 +1,31 @@
-import { MliFormCol, MliFormRow } from '@mli-csmo/base'
+import { halfWidthProps, alnumProps, fullWidthProps } from '@/utils/StringUtil/StringUtil'
+import { SearchOutlined } from '@ant-design/icons'
 import ProForm, { ProFormInstance, ProFormText } from '@ant-design/pro-form'
 import { FooterToolbar } from '@ant-design/pro-layout'
+import { MliFormCol, MliFormRow } from '@mli-csmo/base'
 import { Button, Input, message, Space, Tooltip, Typography } from 'antd'
-import React, { useEffect, useRef, useState } from 'react'
-import { isGuiNumberValid, isNationalIdentificationNumberValid, isResidentCertificateNumberValid } from 'taiwan-id-validator'
-import { debounce } from 'lodash'
 import { log } from 'console'
-import { SearchOutlined } from '@ant-design/icons'
+import { debounce } from 'lodash'
+import React, { useEffect, useRef, useState } from 'react'
+import {
+  isGuiNumberValid,
+  isNationalIdentificationNumberValid,
+  isResidentCertificateNumberValid
+} from 'taiwan-id-validator'
 import OptionReceiveNo from './Components/optionRecevieNo'
-import { onlyAlnum, toFullWidth, toHalfWidth } from '@/utils/StringUtil/StringUtil'
 
 // 模擬數據
 let data = {}
 
 const MyForm: React.FC = () => {
   const formRef = useRef<ProFormInstance>()
-  const [showModal, setShowModal] = useState(false)     // modal 開關
+  const [showModal, setShowModal] = useState(false) // modal 開關
+  const [lock, setLock] = React.useState(false)
 
   useEffect(() => {
     // 預設帶入表單資料
     formRef.current?.setFieldsValue({
-      ...data,
+      ...data
     })
   }, [])
 
@@ -30,14 +35,14 @@ const MyForm: React.FC = () => {
       render: () => (
         <FooterToolbar>
           <Button
-            type='primary'
+            type="primary"
             onClick={async () => {
               log('表單數據', data)
               formRef.current?.validateFields().then(() => {
                 message.success('表單提交成功！')
               })
             }}
-            key='save'
+            key="save"
           >
             確認
           </Button>
@@ -61,7 +66,6 @@ const MyForm: React.FC = () => {
       ...values
     }
   }, 300)
-
 
   /**
    * 身份證字號檢核: 使用 taiwan-id-validator
@@ -89,7 +93,7 @@ const MyForm: React.FC = () => {
       <Typography.Title level={3}>ProFormText</Typography.Title>
       <ProForm
         grid
-        layout='vertical'
+        layout="vertical"
         formRef={formRef}
         submitter={submitterRender()}
         onValuesChange={handleValueChange}
@@ -97,10 +101,10 @@ const MyForm: React.FC = () => {
         <MliFormRow>
           {/* 案例 1 */}
           <ProFormText
-            name='email'
-            label='電子郵件'
-            tooltip='這是用戶電子郵件'
-            placeholder='請輸入電子郵件'
+            name="email"
+            label="電子郵件"
+            tooltip="這是用戶電子郵件"
+            placeholder="請輸入電子郵件"
             rules={[
               { required: true, message: '必填' },
               { required: true, type: 'email' }
@@ -124,12 +128,9 @@ const MyForm: React.FC = () => {
                   rules={[{ required: true, message: '請輸入地址' }]}
                 >
                   <Input
-                    placeholder="地址" style={{ flex: 3 }}
-                    // 失去焦點時，將全形字元轉為半形
-                    onBlur={(e) => {
-                      const value = e.target.value;
-                      formRef?.current?.setFieldsValue({ address: toHalfWidth(value) });
-                    }}
+                    placeholder="地址"
+                    style={{ flex: 3 }}
+                    {...halfWidthProps}
                   />
                 </ProForm.Item>
               </Space.Compact>
@@ -142,28 +143,25 @@ const MyForm: React.FC = () => {
           <ProFormText
             name="clientId"
             label="客戶證號"
-            tooltip='僅能輸入英數'
+            tooltip="僅能輸入英數"
             placeholder=""
-            rules={[
-              { required: true, message: '必填' },
-              { validator: checkClientId }
-            ]}
+            rules={[{ required: true, message: '必填' }, { validator: checkClientId }]}
             fieldProps={{
               maxLength: 10,
               onChange: (e) => {
                 // 輸入文字轉換: 只允許輸入英數字，並強制轉大寫
                 const upper = e.target.value
                   .replace(/[^a-zA-Z0-9]/g, '') // 只允許輸入英數字
-                  .toUpperCase()  // 強制轉大寫
+                  .toUpperCase() // 強制轉大寫
                 // 更新數值
                 formRef?.current?.setFieldValue('clientId', upper)
                 handleValueChange()
-              },
+              }
             }}
           />
           {/* 案例4 */}
           <ProFormText
-            name='receiveNo'
+            name="receiveNo"
             // label='受理號碼'
             label={
               <span>
@@ -171,7 +169,7 @@ const MyForm: React.FC = () => {
                 <Typography.Text style={{ color: 'blue' }}>（按 F6 或 圖標 查詢）</Typography.Text>
               </span>
             }
-            placeholder='請輸入受理號碼'
+            placeholder="請輸入受理號碼"
             rules={[
               {
                 required: true,
@@ -179,21 +177,19 @@ const MyForm: React.FC = () => {
               }
             ]}
             fieldProps={{
-              // 透過 後置圖標 設定 查詢按鈕  
+              // 透過 後置圖標 設定 查詢按鈕
               suffix: (
-                <Tooltip title='查詢'>
-                  <SearchOutlined
-                    onClick={() => setShowModal(true)}
-                  />
+                <Tooltip title="查詢">
+                  <SearchOutlined onClick={() => setShowModal(true)} />
                 </Tooltip>
               ),
               // 監聽鍵盤事件
               onKeyDown: (e) => {
                 if (e.key === 'F6') {
-                  e.preventDefault(); // 防止 F6 的瀏覽器預設行為
-                  setShowModal(true);
+                  e.preventDefault() // 防止 F6 的瀏覽器預設行為
+                  setShowModal(true)
                 }
-              },
+              }
             }}
           />
           <OptionReceiveNo
@@ -209,41 +205,29 @@ const MyForm: React.FC = () => {
             name='halfWidth'
             label='半形測試'
             placeholder=''
-            fieldProps={{ 
+            fieldProps={{
               maxLength: 200,
-              // 失去焦點時，將全形字元轉為半形
-              onBlur: (e) => {
-                const value = e.target.value;
-                formRef?.current?.setFieldsValue({ halfWidth: toHalfWidth(value) });
-              }
+              ...halfWidthProps
             }}
           />
           {/* 案例 6 */}
           <ProFormText
-            name='fullWidth'
-            label='全形測試'
-            placeholder=''
-            fieldProps={{ 
+            name="fullWidth"
+            label="全形測試"
+            placeholder=""
+            fieldProps={{
               maxLength: 200,
-              // 失去焦點時，將全形字元轉為半形
-              onBlur: (e) => {
-                const value = e.target.value;
-                formRef?.current?.setFieldsValue({ fullWidth: toFullWidth(value) });
-              }
+              ...fullWidthProps
             }}
           />
           {/* 案例 7 */}
           <ProFormText
-            name='alnum'
-            label='保留英數'
-            placeholder=''
-            fieldProps={{ 
+            name="alnum"
+            label="保留英數"
+            placeholder=""
+            fieldProps={{
               maxLength: 200,
-              // 失去焦點時，保留英數 字元，並轉大寫
-              onBlur: (e) => {
-                const value = e.target.value;
-                formRef?.current?.setFieldsValue({ alnum: onlyAlnum(value, true) });
-              }
+              ...alnumProps
             }}
           />
         </MliFormRow>
