2a3f9dc6a1c6c927a5b388c95e57677e2b152846
ray0305 <ray0305@gmail.com>
2026-02-22 15:36:12 +0800

    no message

--- csmo-web-demo/packages/app-demo/src/pages/AntdDemo/Sample/Edit/Example/Api/EditDemoController.ts
+++
@@ -1,9 +0,0 @@
-export const getAllData = async (): Promise<any> => {
-  await new Promise((res) => setTimeout(res, 100)) // 模擬延遲
-
-  const data = {
-    sampleText: '這是示範資料'
-  }
-
-  return data
-}
