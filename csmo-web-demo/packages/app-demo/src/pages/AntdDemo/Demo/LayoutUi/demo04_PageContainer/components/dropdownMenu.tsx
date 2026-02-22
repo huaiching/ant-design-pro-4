ba180ee39fdc5dd733fa131329f6460ba2ef9a22
ray0305 <ray0305@gmail.com>
2026-02-22 23:33:47 +0800

    no message

--- csmo-web-demo/packages/app-demo/src/pages/AntdDemo/Demo/LayoutUi/demo04_PageContainer/components/dropdownMenu.tsx
+++ csmo-web-demo/packages/app-demo/src/pages/AntdDemo/Demo/LayoutUi/demo04_PageContainer/components/dropdownMenu.tsx
@@ -1,18 +1,13 @@
 import { Menu } from 'antd'
 import React from 'react'
-import { useNavigate } from 'react-router-dom'
 
 const DropdownMenu: React.FC =() => {
-  const navigate = useNavigate()
-
   const menuClick = (info: any) => {
     if (info.key === '1') {
-      navigate('?activeKey=ProFormText')         // 修改網址
-      window.open(window.location.href, '_blank') // 頁面跳轉 (開新視窗)
+      window.open('/container/demo/antdDemo/Demo/Form/ProFormText')
     }
     if (info.key === '2') {
-      navigate('?activeKey=ProFormSelect')
-      window.open(window.location.href, '_blank')
+      window.open('/container/demo/antdDemo/Demo/Form/ProFormSelect')
     }
   }
 
--- csmo-web-demo/packages/app-demo/src/pages/AntdDemo/Demo/LayoutUi/demo09_ConfigProvider/index.tsx
+++ csmo-web-demo/packages/app-demo/src/pages/AntdDemo/Demo/LayoutUi/demo09_ConfigProvider/index.tsx
@@ -1,44 +1,49 @@
 import { MliFormRow } from '@mli-csmo/base'
 import ProForm, { ProFormDigit, ProFormInstance, ProFormText, ProFormSelect } from '@ant-design/pro-form'
 import { FooterToolbar, PageContainer } from '@ant-design/pro-layout'
-import { Button, ConfigProvider, List, message, Space, Switch, Typography } from 'antd'
-import React, { useRef, useState } from 'react'
+import { Button, ConfigProvider, List, message, Modal, Space, Switch, Typography } from 'antd'
+import React, { useEffect, useRef, useState } from 'react'
 
 const MyForm: React.FC = () => {
   const formRef = useRef<ProFormInstance>()
   const [disabled, setDisabled] = useState<boolean>(false)
 
-  const submitterRender = () => ({
-    render: () => (
-      <FooterToolbar>
-        <Space>
-          <Button
-            type="primary"
-            onClick={async () => {
-              try {
-                const values = await formRef.current?.validateFields()
-                console.info('表單值:', formRef.current?.getFieldsValue())
-                message.success(`表單提交成功！${JSON.stringify(values)}`)
-              } catch (error) {
-                message.error('表單驗證失敗，請檢查輸入')
-              }
-            }}
-            key="save"
-          >
-            送出
-          </Button>
-          <Button
-            onClick={() => {
-              message.info('取消作業')
-            }}
-            key="cancel"
-          >
-            取消
-          </Button>
-        </Space>
-      </FooterToolbar>
-    ),
-  })
+  // 初始載入時的設定
+  useEffect(() => {
+    // 讀取資料
+    // ...
+
+    // 離開頁面前的處理
+    return () => {
+      // 離開頁面前先將資料塞回 mobx
+      handleValueChange()
+    }
+  }, []);
+
+  // 表單值變更處理: 同步更新 Mobx 資料
+  const handleValueChange = () => {
+    const values = formRef.current?.getFieldsValue()
+    // 呼叫 Mobx 的 setting
+  }
+
+  // 控制送出後之動作
+  const submitterRender = () => {
+    Modal.confirm({
+      content: "確定要送出嗎？",
+      onOk() {
+        formRef.current?.validateFields().then(() => {
+          const formRefData = formRef.current?.getFieldsValue()
+          console.log('表單數據', formRefData);
+
+          message.success('表單提交成功！')
+        })
+      },
+      onCancel() {
+        // 取消按鈕 點擊後 要進行的 API 操作
+        message.warning('取消作業')
+      }
+    })
+  }
 
   return (
     <PageContainer
@@ -63,7 +68,7 @@ const MyForm: React.FC = () => {
             grid
             layout="vertical"
             formRef={formRef}
-            submitter={submitterRender()}
+            submitter={false}
           >
             <MliFormRow>
               <ProFormText
@@ -160,6 +165,11 @@ const MyForm: React.FC = () => {
             renderItem={(item) => <List.Item>{item}</List.Item>}
           />
         </ConfigProvider>
+
+        {/* 底部功能區 */}
+        <FooterToolbar>
+          <Button type='primary' onClick={submitterRender}>送出</Button>
+        </FooterToolbar>
       </ProForm>
     </PageContainer >
   )
