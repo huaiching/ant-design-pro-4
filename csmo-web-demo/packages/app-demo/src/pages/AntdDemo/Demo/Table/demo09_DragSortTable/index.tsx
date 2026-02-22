bc9de28fa0809965fe7cf0d1b686e25617d6fc74
ray0305 <ray0305@gmail.com>
2026-02-22 17:35:22 +0800

    no message

--- csmo-web-demo/packages/app-demo/src/pages/AntdDemo/Demo/Table/demo09_DragSortTable/index.tsx
+++ csmo-web-demo/packages/app-demo/src/pages/AntdDemo/Demo/Table/demo09_DragSortTable/index.tsx
@@ -1,27 +1,41 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { DragSortTable, FooterToolbar, PageContainer, ProForm } from '@ant-design/pro-components'
-import type { ProColumns } from '@ant-design/pro-components'
-import { Button, message } from 'antd'
-import * as poApi from './store/poApi' // API 模組（從後端取得保單資料）
-import dayjs from 'dayjs' // 處理日期用的函式庫
+import type { ProColumns, ProFormInstance } from '@ant-design/pro-components'
+import { Button, message, Modal } from 'antd'
+import { fetchAllData } from './Store/poApi'
+import { dayjsToRocString, rocStringToDayjs } from '@/utils/Dayjs/rocDateUtils'
 
 
 
 const DragSortTableExample: React.FC = () => {
+  const formRef = useRef<ProFormInstance>()
+
+  // 數據源
   const [dataSource, setDataSource] = useState<any[]>([]) // 表格資料狀態（主表清單）
 
-  // 🔁 組件載入後，呼叫 API 取得資料
-  useEffect(() => {
-    poApi.fetchAllData().then((data) => {
-      const chgData = data.map((e: any, index: number) => ({
-        ...e,
-        key: e.policyNo ?? index,                         // 每筆資料需有唯一 key，否則表格無法正常渲染
-        poIssueDate: dayjs(e.poIssueDate, 'TTT/MM/DD')   // 將日期字串轉成 dayjs 物件
-      }))
-      setDataSource(chgData)                              // 設定表格顯示資料
-    })
-  }, [])
+  // 查詢 API 設定
+  const requestApi = async () => {
+    const res = await fetchAllData()
+    // 資料格式轉換: 如果有 日期 資料，要轉為 Dayjs 格式，才能正確顯示在 ProTable 的 date 欄位
+    const output = res.map((e: any) => ({
+      ...e,
+      poIssueDate: rocStringToDayjs(e.poIssueDate)
+    }))
+    formRef.current?.setFieldValue('dragTable', output)
+    setDataSource(output)
+  }
 
+  // 初始抓取資料
+  useEffect(() => {
+    requestApi()
+  }, []);
+
+  // formRef 資料同步更新
+  useEffect(() => {
+    formRef.current?.setFieldValue('dragTable', dataSource)
+  }, [dataSource]);
+
+  // 表格欄位定義
   const columns: ProColumns<any>[] = [
     {
       title: '排序',
@@ -48,36 +62,34 @@ const DragSortTableExample: React.FC = () => {
     }
   ]
 
+  // 控制送出後之動作
   const submitterRender = () => {
-    return {
-      render: () => (
-        <FooterToolbar>
-          <Button
-            type='primary'
-            onClick={async () => {
-              try {
-                console.info('✅ 表單內容：', dataSource)
-                message.success('表單提交成功！')
-              } catch (err) {
-                message.error('請檢查表單錯誤')
-              }
-            }}
-            key='save'
-          >
-            確認
-          </Button>
-          <Button
-            onClick={() => {
-              message.warning('取消作業')
-            }}
-          >
-            取消
-          </Button>
-        </FooterToolbar>
-      )
-    }
+    Modal.confirm({
+      content: "確定要送出嗎？",
+      onOk() {
+        // 取得資料
+        const data = formRef.current?.getFieldValue('dragTable')
+        // 日期轉換為民國年
+        const newData = data.map((e: any) => ({
+          ...e,
+          poIssueDate: dayjsToRocString(e.poIssueDate)
+        }))
+        console.info('表單內容：', newData)
+        message.success('表單提交成功！')
+      },
+      onCancel() {
+        // 取消按鈕 點擊後 要進行的 API 操作
+        message.warning('取消作業')
+      }
+    })
   }
 
+  // 排序後事件
+  const onDragSortEnd = (beforeIndex: number, afterIndex: number, newDataSource: any) => {
+    console.info('排序后的数据', newDataSource)
+    setDataSource(newDataSource) // 更新資料狀態
+    message.success('修改列表排序成功') // 顯示提示訊息
+  }
 
   return (
     <PageContainer
@@ -86,29 +98,32 @@ const DragSortTableExample: React.FC = () => {
       }}
     >
       <ProForm
-        layout="vertical"       // 垂直排列 label + input
-        submitter={submitterRender()}
+        formRef={formRef}
+        layout="vertical"
+        submitter={false}
       >
         <DragSortTable
-          headerTitle="拖拽排序"                 // 表格標題
-          columns={columns}                     // 表格欄位
-          rowKey="key"                          // 每筆資料的唯一識別欄位
-          search={false}                        // 關閉搜尋欄
-          pagination={false}                    // 關閉分頁功能
-          dataSource={dataSource}               // 資料來源（直接綁定 state）
-          cardProps={false}                     //  移除 Card 包裝
-          dragSortKey="sort"                    // 拖曳排序的對應欄位（自動顯示拖曳把手）
-          // ✅ 拖曳排序完成後觸發：更新畫面與提示訊息
-          onDragSortEnd={(
-            beforeIndex: number,  // 拖曳前的 index
-            afterIndex: number,   // 拖曳後的 index
-            newDataSource: any    // 排序後的資料陣列
-          ) => {
-            console.info('排序后的数据', newDataSource)
-            setDataSource(newDataSource) // 更新資料狀態
-            message.success('修改列表排序成功') // 顯示提示訊息
-          }}
+          headerTitle="拖拽排序"
+          rowKey="key"
+          columns={columns}
+          cardProps={false} // 移除外層 Card
+          size='small'
+          // 數據源
+          dataSource={dataSource}
+          // 搜尋欄
+          search={false}
+          // 分頁
+          pagination={false}
+          // 排序欄位
+          dragSortKey="sort"
+          // 拖曳後事件
+          onDragSortEnd={onDragSortEnd}
         />
+
+        {/* 底部功能區 */}
+        <FooterToolbar>
+          <Button type='primary' onClick={submitterRender}>送出</Button>
+        </FooterToolbar>
       </ProForm>
     </PageContainer >
   )
