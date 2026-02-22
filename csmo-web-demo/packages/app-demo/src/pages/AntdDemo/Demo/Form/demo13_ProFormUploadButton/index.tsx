f461a205311e4ca1559bd2aaf505b818d3571170
ray0305 <ray0305@gmail.com>
2026-02-22 21:42:38 +0800

    no message

--- csmo-web-demo/packages/app-demo/src/pages/AntdDemo/Demo/Form/demo13_ProFormUploadButton/index.tsx
+++ csmo-web-demo/packages/app-demo/src/pages/AntdDemo/Demo/Form/demo13_ProFormUploadButton/index.tsx
@@ -2,62 +2,52 @@
 import React, { useEffect, useRef } from 'react'
 import ProForm, { ProFormInstance, ProFormUploadButton } from '@ant-design/pro-form'
 import { FooterToolbar, PageContainer } from '@ant-design/pro-layout'
-import { Button, message } from 'antd'
+import { Button, message, Modal } from 'antd'
 import { MliFormRow } from '@mli-csmo/base'
-import { log } from 'console'
-import { debounce } from 'lodash'
-
-// 模擬數據
-let data = {}
 
 const MyForm: React.FC = () => {
   const formRef = useRef<ProFormInstance>()
 
+  // 初始載入時的設定
   useEffect(() => {
-    // 預設帶入表單資料
-    formRef.current?.setFieldsValue({
-      ...data,
-    })
-  }, [])
+    // 讀取資料
+    // ...
+
+    // 離開頁面前的處理
+    return () => {
+      // 離開頁面前先將資料塞回 mobx
+      handleValueChange()
+    }
+  }, []);
+
+  // 表單值變更處理: 同步更新 Mobx 資料
+  const handleValueChange = () => {
+    const values = formRef.current?.getFieldsValue()
+    // 呼叫 Mobx 的 setting
+  }
 
   // 控制送出後之動作
   const submitterRender = () => {
-    return {
-      render: () => (
-        <FooterToolbar>
-          <Button
-            type='primary'
-            onClick={async () => {
-              log('表單數據', data)
-              formRef.current?.validateFields().then(() => {
-                message.success('表單提交成功！')
-              })
-            }}
-            key='save'
-          >
-            確認
-          </Button>
-          <Button
-            onClick={async () => {
-              // 取消按鈕 點擊後 要進行的 API 操作
-              message.warning('取消作業')
-            }}
-          >
-            取消
-          </Button>
-        </FooterToolbar>
-      )
-    }
-  }
+    Modal.confirm({
+      content: "確定要送出嗎？",
+      onOk() {
+        formRef.current?.validateFields().then(() => {
+          const formRefData = formRef.current?.getFieldsValue()
+          const file = formRef.current?.getFieldsValue().file[0]
 
-  // 表單值變更處理，使用 debounce 限制觸發頻率
-  const handleValueChange = debounce(() => {
-    // 取得表單變更資料
-    const values = formRef.current?.getFieldsValue()
-    data = {
-      ...values
-    }
-  }, 300)
+          console.log('表單數據', formRefData);
+          console.log('file', file);
+          console.log('fileData', file.originFileObj);
+
+          message.success('表單提交成功！')
+        })
+      },
+      onCancel() {
+        // 取消按鈕 點擊後 要進行的 API 操作
+        message.warning('取消作業')
+      }
+    })
+  }
 
   return (
     <PageContainer>
@@ -65,7 +55,7 @@ const MyForm: React.FC = () => {
         grid
         layout='vertical'
         formRef={formRef}
-        submitter={submitterRender()}
+        submitter={false}
         onValuesChange={handleValueChange}
       >
         <MliFormRow>
@@ -74,11 +64,16 @@ const MyForm: React.FC = () => {
             label='上傳文件'
             title='上傳'
             fieldProps={{
-              maxCount: 1       // 最多只能上傳一個文件
+              maxCount: 1,
             }}
             rules={[{ required: true, message: '請上傳文件' }]} // 校驗規則，要求文件必須上傳
           />
         </MliFormRow>
+
+        {/* 底部功能區 */}
+        <FooterToolbar>
+          <Button type='primary' onClick={submitterRender}>送出</Button>
+        </FooterToolbar>
       </ProForm>
     </PageContainer>
   )
